add0b7fd73167f0ab6b75445a9584a84fd6f248a
Yasin Silavi <ys.silavi@gmail.com>
2025-01-05 17:58:40 +0330

    feat: remove expand button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,18 @@
 "use client";
 
-import { isClientSide } from "@/utils/environment";
+import { useEffect } from "react";
 
 export default function Home() {
-  const onExpand = () => {
-    if (!isClientSide()) {
-      return;
-    }
-
+  useEffect(() => {
+    // Expand mini app bottom-sheet on app open
     window.Eitaa.WebApp.expand();
-  };
+  });
 
   return (
     <main className="h-svh bg-black text-white">
       <div className="h-full flex flex-col justify-center items-center">
         <h1 className="text-7xl font-bold">TALAZOOD</h1>
         <span className="text-gray-400 mt-4">COMING SOON</span>
-        <button
-          onClick={onExpand}
-          className="border border-white py-4 px-12 mt-16"
-        >
-          Expand Button
-        </button>
       </div>
     </main>
   );
