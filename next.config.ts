c139528a1d37d838562a044f072dc57ec0d2f573
Yasin Silavi <ys.silavi@gmail.com>
2025-01-06 00:09:57 +0330

    chore: remove base-path option

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import { isDev } from "@/utils/environment";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: !isDev() ? "/frontend" : "",
 };
 
 export default nextConfig;
