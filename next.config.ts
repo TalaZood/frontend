ce575c6c288a7fcb15703b5b93fa6bb33d648f5b
Yasin Silavi <ys.silavi@gmail.com>
2025-01-05 15:09:13 +0330

    chore: change base-path on production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { isDev } from "@/utils/environment";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: !isDev() ? "/frontend" : "",
 };
 
 export default nextConfig;
