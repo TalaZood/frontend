86e34b621d4f4ba2b63ec121f0b4fde74b2bd4ad
Yasin Silavi <ys.silavi@gmail.com>
2025-01-06 09:18:47 +0330

    feat: implement http module WIP

---
+++ src/lib/utils/http/index.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+interface RequestOptions extends RequestInit {
+  throwError?: boolean;
+  params?: Record<string, any>;
+}
+
+async function request<T>(url: string, options: RequestOptions = {}) {
+  options.headers = {
+    "Content-Type": "application/json",
+    ...options.headers,
+  };
+
+  const params = options.params
+    ? `?${new URLSearchParams(options.params || "").toString()}`
+    : "";
+  const fullUrl = process.env.NEXT_PUBLIC_API_BASE_URL + url + params;
+
+  try {
+    const response = await fetch(fullUrl, options);
+
+    if (!response.ok) {
+      throw response;
+    }
+
+    const result = await response.json();
+
+    return result as T;
+  } catch (error) {
+    console.log("error", error);
+
+    return "NOT HANDLED ERROR";
+  }
+}
+
+export const http = {
+  get: <T>(url: string, options?: RequestOptions) => {
+    return request<T>(url, { method: "GET", ...options });
+  },
+  post: <T>(url: string, options?: RequestOptions) => {
+    return request<T>(url, {
+      method: "POST",
+      body: JSON.stringify(options.body),
+      ...options,
+    });
+  },
+};
