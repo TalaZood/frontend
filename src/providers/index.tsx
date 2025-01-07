ade26ac08aef04f6d06c268d12ea7ce8f4ac7929
Yasin Silavi <ys.silavi@gmail.com>
2025-01-08 00:58:36 +0330

    fix: wrap mini-app-provider inside suspense component

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,13 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Suspense } from "react";
 import QueryProvider from "./QueryProvider";
 import MiniAppProvider from "./MiniAppProvider";
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <QueryProvider>
-      <MiniAppProvider>{children}</MiniAppProvider>
+      <Suspense>
+        <MiniAppProvider>{children}</MiniAppProvider>
+      </Suspense>
     </QueryProvider>
   );
 }
